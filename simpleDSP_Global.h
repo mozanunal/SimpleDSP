1fedbf1992f96ef5fd1d2d2fbb21e8ac2dd285de
Mehmet Ozan Ünal <mozanunal@users.noreply.github.com>
2017-01-02 20:52:18 +0200

    Create simpleDSP_Global.h

---
+++ simpleDSP_Global.h
@@ -0,0 +1,12 @@
+/* begin simpleDSP_Global.h */
+#ifndef _SIMPLEDSP_GLOBAL_H
+#define _SIMPLEDSP_GLOBAL_H
+
+struct cmpx
+  {
+  double real;
+  double imag;
+  };
+typedef struct cmpx COMPLEX;
+
+#endif  /* end simpleDSP_Global.h */
