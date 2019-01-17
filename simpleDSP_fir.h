23a66a72160ad6576967a88dfcbab86a2edb9112
Mehmet Ozan Ünal <mehmetozan.unal@basestech.com>
2019-01-18 00:08:40 +0300

    extern c and naming

--- simpleDSP_fir.h
+++ simpleDSP_fir.h
@@ -2,6 +2,10 @@
 #ifndef _SIMPLEDSP_FIR_H
 #define _SIMPLEDSP_FIR_H
 
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
 #include "simpleDSP_Global.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +17,11 @@ typedef struct
     float *dlyX;
 }FIR;
 
-void init(FIR *fir, int coefBLen, float *coefsB);
-float filt(FIR *fir, int input);
+void firInit(FIR *fir, int coefBLen, float *coefsB);
+float firFilt(FIR *fir, int input);
+
+#ifdef __cplusplus
+  }
+#endif
 
 #endif /*end simpleDSP_fir.h */
